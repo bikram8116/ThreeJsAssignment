cb398fa0c808fb1d1d8aa04837c5e4c929c592c6
bikram8116 <bikram7586@gmail.com>
2022-06-12 07:14:34 -0400

    done

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
